8b0292c85904690556a4074c860585dcc86a26e7
Ewerton <ewerton2004@yahoo.com.br>
2024-12-08 00:11:53 -0300

    feat: style bottom tab

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,19 +5,48 @@ const { Navigator, Screen } = createBottomTabNavigator();
 import HomeScreen from '../screens/home/HomeScreen';
 import SkillsScreen from '../screens/skills/SkillsScreen';
 
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import colors from '../styles/colors';
+
 export default function Routes() {
   return (
-    <Navigator>
+    <Navigator 
+    screenOptions={{
+      headerShown: false,
+      headerBackgroundContainerStyle: {
+        backgroundColor: colors.terciary
+      },
+      tabBarStyle: {
+        height: 64,
+        borderTopWidth: 0
+      },
+      tabBarLabelStyle: {
+        fontSize: 16,
+        fontWeight: 300,
+      },
+      tabBarInactiveTintColor: "#2D4062",
+      tabBarActiveTintColor: colors.terciary,
+      tabBarInactiveBackgroundColor: colors.primary,
+      tabBarActiveBackgroundColor: "#202E46",
+    }}
+    >
       <Screen 
-        name="Home" component={HomeScreen} 
+        name="Home" 
+        component={HomeScreen} 
         options={{
-        headerShown: false
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="home" size={size} color={color}/>
+          )
         }}
       />
       <Screen 
-        name="Skills" component={SkillsScreen} 
+        name="Skills" 
+        component={SkillsScreen} 
         options={{
-        headerShown: false
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="build" size={size} color={color}/>
+          )
         }}
       />
     </Navigator>
